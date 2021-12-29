acb1e4be0d5ac8abf9ee911421d246d245e4510d
caizhanfei <caizhanfei@126.com>
2021-12-29 17:02:36 -0500

    005

--- solutions/005_GC.py
+++ solutions/005_GC.py
@@ -1,13 +1,28 @@
-f = open('data.fasta')
+f = open('rosalind_gc.txt')
 lines = f.readlines()
 name = ''
 content = ''
 dnas = dict()
 for line in lines:
+    line = line.strip()
     if line.startswith('>'):
         dnas[name] = content
         name = line.replace('>', '')
         content = ''
         continue
     content += line
-print(dnas)
+dnas[name] = content
+
+id = ''
+gc_content = 0
+for key, value in dnas.items():
+    value = value.strip()
+    if not key.startswith('Rosalind_'):
+        continue
+    count = value.count('G')+value.count('C')
+    percentage = round((count/len(value))*100, 6)
+    if percentage > gc_content:
+        id = key
+        gc_content = percentage
+print(id)
+print(gc_content)
